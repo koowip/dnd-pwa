be92ce8801cb5eb082011f7306742e43c956bfbd
pat <patrickwhite.swe@gmail.com>
2024-05-08 14:11:05 -0700

    subclasses added, working on how to capture state and filtering by that state

--- src/components/FilterSheet.tsx
+++ src/components/FilterSheet.tsx
@@ -12,7 +12,12 @@ import ToggleButton from "./ToggleButton";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { useState, useEffect } from "react";
 
-const FilterSheet = ({ isSelectedClass, setIsSelectedClass }: any) => {
+const FilterSheet = ({
+  isSelectedClass,
+  setIsSelectedClass,
+  isSelectedSubClass,
+  setIsSelectedSubClass,
+}: any) => {
   const handleToggle = (className: any) => {
     setIsSelectedClass((prevState: { [x: string]: any }) => ({
       ...prevState,
@@ -20,29 +25,50 @@ const FilterSheet = ({ isSelectedClass, setIsSelectedClass }: any) => {
     }));
   };
 
-    useEffect(() => {
-      console.log(isSelectedClass);
-    }, [isSelectedClass]);
+  //This is here becuase of the stupid rendering when state changes being async,
+  //This forces the state is be updated when button is clicked, not queued for later
+  useEffect(() => {}, [isSelectedClass]);
 
-    return (
-      <Sheet>
-        <SheetTrigger>
-          <HamburgerMenuIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
-        </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>Header</SheetHeader>
+  function getSelectedSubclasses(className = null) {
+    //debugger
+    console.log(isSelectedSubClass[className])
+    return isSelectedSubClass[className] || [];
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <HamburgerMenuIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>Header</SheetHeader>
+        <div className="flex flex-wrap">
+          {Object.entries(isSelectedClass).map(([classLabel, isSelected]) => (
+            <ToggleButton
+              key={classLabel}
+              classLabel={classLabel}
+              isSelected={isSelected}
+              onToggle={() => handleToggle(classLabel)}
+            />
+          ))}
+        </div>
+        <div>
+          <strong>Subclasses</strong>
           <div className="flex flex-wrap">
-            {Object.entries(isSelectedClass).map(([classLabel, isSelected]) => (
-              <ToggleButton
-                key={classLabel}
-                classLabel={classLabel}
-                isSelected={isSelected}
-                onToggle={() => handleToggle(classLabel)}
-              />
-            ))}
+            {Object.entries(getSelectedSubclasses("Bard")).map(
+              ([subClassLabelIndex, isSelected]) => (
+                <ToggleButton
+                  key={isSelected[0]}
+                  classLabel={isSelected[0]}
+                  isSelected={isSelected}
+                  onToggle={() => handleToggle(subClassLabelIndex)}
+                />
+              )
+            )}
           </div>
-        </SheetContent>
-      </Sheet>
-    );
-  };
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 export default FilterSheet;
