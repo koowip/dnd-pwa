e05615fb06ce1db0be8f844a39a4a5e334d7af66
pat <patrickwhite.swe@gmail.com>
2024-05-07 20:57:53 -0700

    Who even knows anymore honestly

--- src/components/FilterSheet.tsx
+++ src/components/FilterSheet.tsx
@@ -13,34 +13,36 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { useState, useEffect } from "react";
 
 const FilterSheet = ({ isSelectedClass, setIsSelectedClass }: any) => {
-
   const handleToggle = (className: any) => {
-    setIsSelectedClass((prevState: { [x: string]: any; }) => ({
+    setIsSelectedClass((prevState: { [x: string]: any }) => ({
       ...prevState,
-      [className]: !prevState[className]
-    }))
-  }
+      [className]: !prevState[className],
+    }));
+  };
 
-  return (
-    <Sheet>
-      <SheetTrigger>
-        <HamburgerMenuIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
-      </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>Header</SheetHeader>
-        <div className="flex flex-wrap">
-          {Object.entries(isSelectedClass).map(([classLabel, isSelected]) => (
-            <ToggleButton
-              key={classLabel}
-              classLabel={classLabel}
-              isSelected={isSelected}
-              onToggle={() => handleToggle(classLabel)}
-            />
-          ))}
-        </div>
-      </SheetContent>
-    </Sheet>
-  );
-};
+    useEffect(() => {
+      console.log(isSelectedClass);
+    }, [isSelectedClass]);
 
+    return (
+      <Sheet>
+        <SheetTrigger>
+          <HamburgerMenuIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>Header</SheetHeader>
+          <div className="flex flex-wrap">
+            {Object.entries(isSelectedClass).map(([classLabel, isSelected]) => (
+              <ToggleButton
+                key={classLabel}
+                classLabel={classLabel}
+                isSelected={isSelected}
+                onToggle={() => handleToggle(classLabel)}
+              />
+            ))}
+          </div>
+        </SheetContent>
+      </Sheet>
+    );
+  };
 export default FilterSheet;
--- src/lib/services/SpellService.ts
+++ src/lib/services/SpellService.ts
@@ -1,14 +1,18 @@
-import SpellsList from '../../AllSpells.json'
-import { Spell } from '../types'
+import SpellsList from "../../AllSpells.json";
+import { Spell } from "../types";
 
-// interface ISpellService {
-//   List: (name: string) => Spell[];
-// }
+/* Filter criteria
+  By base class //Done
+  By subclass
+  By spell level
+  By variant
+*/
 
+//Main function that takes a filtered list of spell names and returns a list of spells to be rendered by SpellAccordion
 export function List(name: string): any {
-
-  return SpellsList.filter(x => x.name.toLowerCase().includes(name.toLowerCase()));
-
+  return SpellsList.filter((x) =>
+    x.name.toLowerCase().includes(name.toLowerCase())
+  );
 }
 
 //Return not only class spells but subclass only spells
@@ -16,9 +20,12 @@ export function List(name: string): any {
 export function FilterClassNonOpinionated(specifiedClasses: {}, curSpell: Spell): any {
   //return Object.keys(curSpell.availableTo).includes(selectedClasses); //OG code
 
-  const selectedClasses = Object.keys(specifiedClasses).filter(key => specifiedClasses[key]);
-  return selectedClasses.some(selectedClasses => Object.keys(curSpell.availableTo).includes(selectedClasses));
-  
+  const selectedClasses = Object.keys(specifiedClasses).filter((key) => specifiedClasses[key]);
+  if (selectedClasses) {
+    return selectedClasses.some((selectedClasses) => Object.keys(curSpell.availableTo).includes(selectedClasses));
+  }
+  else 
+    return true;
 }
 
 //Returns only base class spells, subclass spells are not included
@@ -26,7 +33,33 @@ export function FilterClassNonOpinionated(specifiedClasses: {}, curSpell: Spell)
 export function FilterClass(specifiedClasses: {}, curSpell: Spell): any {
   //return Object.keys(curSpell.availableTo).filter(x => curSpell.availableTo[x].length === 0).includes(specifiedClass); //OG code
 
-  const selectedClasses = Object.keys(specifiedClasses).filter(key => specifiedClasses[key]);
-  return selectedClasses.some(selectedClasses => Object.keys(curSpell.availableTo).filter(x => curSpell.availableTo[x].length === 0).includes(selectedClasses))
-  
+  const selectedClasses = Object.keys(specifiedClasses).filter((key) => specifiedClasses[key]);
+
+  if (selectedClasses) {
+    return selectedClasses.some((selectedClasses) => Object.keys(curSpell.availableTo).filter((x) => curSpell.availableTo[x].length === 0).includes(selectedClasses));
+  } else
+    return true;
+}
+
+export function FilterSubclass(specifiedSubclasses: {}, curSpell: Spell): any {
+
+  const selectedClasses = Object.keys(specifiedClasses).filter((key) => specifiedClasses[key]);
+  return Object.values(curSpell.availableTo).flat().includes("variant")
+  //return true;
+}
+
+export function FilterLevel(specifiedLevel: number, curSpell: Spell): any {
+
+  if(specifiedLevel > -1 )
+    return curSpell.level === specifiedLevel;
+  else
+    return true;
+}
+
+
+export function FilterVariant(curSpell: Spell, variant?: boolean): any {
+  if(variant) {
+    return Object.values(curSpell.availableTo).flat().includes("variant");
+  } else
+    return true;
 }
