695ded40f2efbff9bf0ae88314cca32ddf3d1b9b
Pat <patrickwhite.swe@gmail.com>
2024-05-26 00:34:41 -0700

    Download and upload of favorite spells file and all spells file done. Needs further checking tho. TODO: figure out toast notifications for when upload is done and download is done

--- src/components/FileDialog.tsx
+++ src/components/FileDialog.tsx
@@ -9,14 +9,91 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
+import { useRef } from "react";
+import useClassStore from "@/lib/services/StoreService";
 
 const FileDialog = () => {
+  const fileInputRef = useRef(null);
+  const { setBookSpellList, setSpellList, bookSpellList } = useClassStore();
+
+  const downloadLocalStorage = () => {
+    if (bookSpellList.length > 0) {
+      // Retrieve the JSON data from localStorage
+      const favSpell = JSON.parse(localStorage.getItem("favoritedSpells"));
+      const allSpell = JSON.parse(localStorage.getItem("allSpells"));
+
+      const bothSpells = {
+        favoritedSpells: favSpell,
+        allSpells: allSpell,
+      };
+
+      const bothSpellsJson = JSON.stringify(bothSpells, null, 2);
+
+      const blob = new Blob([bothSpellsJson], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+
+      // Create a link element and trigger the download
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "combinedSpells.json";
+      document.body.appendChild(link);
+      link.click();
+
+      // Clean up the link element and URL
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+    } else {
+    }
+  };
+
+  const uploadLocalStorage = (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+      console.error("No file selected");
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      try {
+        const jsonData = JSON.parse(e.target.result);
+
+        // Check if jsonData has the expected structure
+        if (jsonData.favoritedSpells && jsonData.allSpells) {
+          // Save the JSON data to localStorage
+
+          localStorage.setItem("allSpells", JSON.stringify(jsonData.allSpells));
+          let localData = localStorage.getItem("allSpells");
+          setSpellList(JSON.parse(localData));
+
+          localStorage.setItem(
+            "favoritedSpells",
+            JSON.stringify(jsonData.favoritedSpells)
+          );
+          localData = localStorage.getItem("favoritedSpells");
+          console.log(localData);
+          setBookSpellList(JSON.parse(localData));
+
+          alert("Data successfully uploaded to localStorage");
+        } else {
+          console.error("Invalid JSON structure");
+          alert("Invalid JSON structure");
+        }
+      } catch (error) {
+        console.error("Error parsing JSON", error);
+        alert("Error parsing JSON");
+      }
+    };
+
+    reader.readAsText(file);
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
         <BsThreeDotsVertical />
       </DialogTrigger>
-      <DialogContent className="rounded-md size-80">
+      <DialogContent className="rounded-md size-90">
         <DialogHeader>
           <DialogTitle>Export / Import Spells</DialogTitle>
           <DialogDescription className="py-2">
@@ -24,9 +101,27 @@ const FileDialog = () => {
             via downloading it locally.
           </DialogDescription>
         </DialogHeader>
-        <Button>Download Book</Button>
-        <Button>Upload Book</Button>
-        <DialogFooter className="text-slate-400 inline">Your DM <span className="text-red-500">&hearts;'s</span> you.</DialogFooter>
+        <Button
+          className={bookSpellList.length > 0 ? "bg-green-600" : "bg-red-700"}
+          onClick={() => downloadLocalStorage()}
+        >
+          {bookSpellList.length > 0
+            ? "Download Book"
+            : "Favorite some spells to enable download"}
+        </Button>
+        <input
+          type="file"
+          ref={fileInputRef}
+          accept=".json"
+          style={{ display: "none" }}
+          onChange={uploadLocalStorage}
+        />
+        <Button onClick={() => fileInputRef.current.click()}>
+          Upload Book
+        </Button>
+        <DialogFooter className="text-slate-400 inline">
+          Your DM <span className="text-red-500">&hearts;</span>'s you.
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
