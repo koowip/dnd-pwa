a9a1ec9ef6ec7594890bc296fbb34f2a882ac830
Patrick <patrickwhite.swe@gmail.com>
2024-06-13 11:14:35 -0700

    pwa files added, index.html changed.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,36 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "SpellBook",
+        short_name: "SpellBook",
+        description: "Pat's Shitty SpellBook App",
+        theme_color: "#FFD059",
+        icons: [
+          {
+            src: "/nat20.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/nat20_resized_transparent.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
